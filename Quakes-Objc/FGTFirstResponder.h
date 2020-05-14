8529b9a50f6c59592ddc5ea11fa0ac049b86ba46
fritzgt <tuanis@live.com>
2020-05-14 11:33:11 +0300

    initial commit

---
+++ Quakes-Objc/FGTFirstResponder.h
@@ -0,0 +1,20 @@
+//
+//  FGTFirstResponder.h
+//  Quakes-Objc
+//
+//  Created by FGT MAC on 5/14/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FGTFirstResponder : NSObject
+
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Quakes-Objc/FGTFirstResponder.m
@@ -0,0 +1,13 @@
+//
+//  FGTFirstResponder.m
+//  Quakes-Objc
+//
+//  Created by FGT MAC on 5/14/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "FGTFirstResponder.h"
+
+@implementation FGTFirstResponder
+
+@end
